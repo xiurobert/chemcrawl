8b94bbaadc22cccc82cb3696ffd6d55c7c4ceace
Robert Xiu <xiurobert@gmail.com>
2021-07-03 16:07:02 +0800

    more stuff

--- bruh.js
+++ bruh.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const bruhg = express();
+const {TwingEnvironment, TwingLoaderFilesystem} = require('twing');
+
+let twing
 
 let conf;
 if (process.env.NODE_ENV === 'development') {
     conf = require("./config.dev.json");
+    twing = new TwingEnvironment(new TwingLoaderFilesystem('./views'), {
+        debug: true,
+        cache: false,
+        auto_reload: true
+    });
 } else {
     conf = require("./config.json");
+    twing = new TwingEnvironment(new TwingLoaderFilesystem('./views'));
 }
 
 // express setup
@@ -13,5 +22,6 @@ bruhg.use(express.static('public'));
 
 module.exports = {
     app: bruhg,
-    config: conf
+    config: conf,
+    twing: twing
 };
--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const mango = require("mongodb");
+const nocache = require("nocache");
 const app = require("./bruh").app;
 const conf = require("./bruh").config;
+const twing = require("./bruh").twing;
+
+app.use(nocache());
+
+app.get('/', function (req, res){
+    twing.render('index.twig',).then(output => res.end(output));
+});
+
 
 app.listen(conf.server.port, () => {
     console.log(`[chemcrawl] Started express server on port ${conf.server.port}`)
 });
+
