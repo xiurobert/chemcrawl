7938bb2f3df03bd7673ceecba22353648998db2d
Robert Xiu <xiurobert@gmail.com>
2021-07-05 17:51:49 +0800

    user creation script works

--- create-user.js
+++ create-user.js
@@ -1,26 +1,38 @@
 const {mongo_client, config} = require("./bruh");
 const bcrypt = require('bcrypt');
 const logging = require('./logging');
+const chalk = require('chalk');
 
 let username = process.argv.slice(2)[0];
 let password = process.argv.slice(2)[1];
 
+console.log(`Using username ${chalk.cyan(username)} and password ${chalk.redBright(password)}`);
+
 mongo_client.connect().then(() => {
     const db = mongo_client.db(config.db.name);
     const coll = db.collection("admins");
-    console.log("here 1");
-    bcrypt.hash(password, 4).then(hashed => {
-        console.log('here 2');
-        coll.insertOne({
-            "username": username,
-            "password": hashed
-        }).then(res => {
-            console.log(`${logging.prefix} ${logging.levels.info} Created user: ${res.insertedId}`);
-            process.exit(0)
-        }).catch(err => {
-            console.log(`${logging.prefix} ${logging.levels.err} Error creating user`);
-            process.exit(1);
+
+    coll.find({username: username}).toArray().then(res => {
+
+        if (res && res.length >= 1) {
+            // user exists
+            console.log(`${logging.levels.err} User exists`);
+            process.exit(1)
+        }
+        bcrypt.hash(password, 4).then(hashed => {
+            coll.insertOne({
+                "username": username,
+                "password": hashed
+            }).then(res => {
+                console.log(`${logging.prefix} ${logging.levels.info} Created user: ${chalk.yellow(res.insertedId)}`);
+                process.exit(0)
+            }).catch(err => {
+                console.log(`${logging.prefix} ${logging.levels.err} Error creating user`);
+                process.exit(1);
+            })
         })
     })
+
+
 
 })
