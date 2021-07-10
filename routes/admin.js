47215ef2a384332a9a2ed2ec8200367dd1528d3f
Robert Xiu <xiurobert@gmail.com>
2021-07-10 20:59:54 +0800

    work on uploading examples

--- routes/admin.js
+++ routes/admin.js
@@ -4,6 +4,10 @@ const {twing, config, mongo_client, db} = require("../bruh");
 const router = express.Router();
 const logging = require("../logging");
 const chalk = require("chalk");
+const mongo = require("mongodb");
+const multer = require("multer");
+
+
 
 router.get('/login', (req, res) => {
     twing.render('admin/login.twig', {
@@ -77,6 +81,32 @@ router.post('/addrxn', (req, res) => {
             res.redirect(`/organic/rxn/${result.insertedId}`);
         })
         
+    })
+
+})
+
+router.get('/addexample/:type/:target_id', (req, res) => {
+    twing.render('admin/add_example.twig', {
+        "app_name": config.app.name,
+        "type": req.params['type'],
+        "target_id": req.params['target_id']
+    }).then(output => res.end(output));
+})
+
+router.post('/addexample/:type/:target_id', multer({dest: `${__dirname}/../upload-tmp`}).single('file'),
+    (req, res) => {
+    let targetId;
+    try {
+        targetId = mongo.ObjectId(req.params['target_id']);
+    } catch {
+        console.log(`${logging.prefix} ${logging.levels.warn} Someone just tried to access an invalid objectid: ${req.params['rxnId']}`);
+        res.end("That is not a valid objectid");
+    }
+    mongo_client.connect().then(() => {
+        const db = mongo_client.db(config.db.name);
+        if (req.params['type'].toLowerCase() === 'organic') {
+
+        }
     })
 
 })
