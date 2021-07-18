7dfec33321e8829061a92fa97747a0fcbf55730b
Robert Xiu <xiurobert@gmail.com>
2021-07-18 18:58:50 +0800

    working on sorting

--- routes/organic.js
+++ routes/organic.js
@@ -5,12 +5,34 @@ const mongo = require("mongodb");
 const logging = require('../logging');
 const ash = require("express-async-handler")
 
-router.get('/rxnlist',  ash(async(req, res) => {
+router.get('/rxn-list/:sortBy-:sortOrder',  ash(async(req, res) => {
+    const sortBy = req.params["sortBy"];
+    const sortOrder = req.params["sortOrder"];
+
+    const hack = {
+        "asc": 1,
+        "dsc": -1
+    }
+
+    if (!["abc", "type", "reagents", "conditions"].includes(sortBy.toLowerCase())) {
+        res.status(400).end("Invalid sort criterion");
+    }
+
+    if (!["asc", "dsc"].includes(sortOrder.toLowerCase())) {
+        res.status(400).end("Invalid sort order");
+    }
+
     await mongo_client.connect();
     const coll = mongo_client.db(config.db.name).collection('organic_reactions');
+
+    let all_da_stuff = await coll.find();
+    let dae_return;
+    if (sortBy.toLowerCase() === 'abc') {
+        dae_return = all_da_stuff.sort({name: -1}).toArray();
+    }
     res.end(await twing.render('organic/list.twig', {
         "app_name": config.app.name,
-        "result": await coll.find().toArray()
+        "result": dae_return
     }));
 }));
 
@@ -25,9 +47,7 @@ router.get('/rxn/:rxnId', ash(async(req, res) => {
     await mongo_client.connect();
     const db = mongo_client.db(config.db.name);
     const coll = db.collection('organic_reactions');
-    const grid = new mongo.GridFSBucket(db);
-
-
+    // const grid = new mongo.GridFSBucket(db);
 
     res.end(await twing.render('organic/rxn_info.twig', {
         "app_name": config.app.name,
