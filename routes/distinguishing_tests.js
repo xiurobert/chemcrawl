c5fe80cdb600b0f56d2d397328422fc1cc6113a3
Robert Xiu <xiurobert@gmail.com>
2021-07-31 22:57:29 +0800

    eh im kinda lazy

--- routes/distinguishing_tests.js
+++ routes/distinguishing_tests.js
@@ -7,12 +7,43 @@ const logging = require("../logging");
 const {mongo_client} = require("../bruh");
 
 router.get('/list/:sortBy-:sortOrder', ash(async (req, res) => {
+    const sortBy = req.params["sortBy"].toLowerCase();
+    const sortOrder = req.params["sortOrder"].toLowerCase();
+
+    const map = {
+        "asc": 1,
+        "dsc": -1
+    }
+
+    if (!["abc", "reagents"].includes(sortBy)) {
+        res.status(400).end("Invalid sort criterion");
+    }
+
+    if (!["asc", "dsc"].includes(sortOrder)) {
+        res.status(400).end("Invalid sort order");
+    }
+
+    await mongo_client.connect();
+    const coll = mongo_client.db(config.db.name).collection('distinguishing_tests');
+
+    let stuff = await coll.find();
+    let retVal;
+
+    if (sortBy === 'abc') {
+        retVal = await stuff.sort({name: map[sortOrder.toLowerCase()]}).toArray();
+    } else if (sortBy === 'reagents') {
+        // TODO: kinda complex
+    }
+
     res.end(await twing.render('tests/list.twig', {
-        "app_name": config.app.name
+        "app_name": config.app.name,
+        "result": retVal,
+        "sortBy": sortBy,
+        "sortOrder": sortOrder
     }));
 }))
 
-router.get('/:testId', ash(async (req, res) => {
+router.get('/test/:testId', ash(async (req, res) => {
     let testId;
     try {
         testId = mongo.ObjectId(req.params['testId']);
